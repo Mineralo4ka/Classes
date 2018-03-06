94bb1ec36e1714168976afeb6d145ecdc86194b0
Mineralo4ka <andrew.pavlenko@mail.ru>
2018-03-06 11:29:33 +0700

    Add func Input
    Wrong with check

---
+++ Classes/Large_Integer.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes
+{
+    class Large_Integer
+    {
+        private int[] number_1 = null, number_2 = null;
+
+        public Large_Integer(string num_1, string num_2)
+        {
+            number_1 = new int[num_1.Length];
+            if (num_1[0] != '\n')
+            {
+                for (int i = 0; i < num_1.Length; i++)
+                {
+                    number_1[i] = int.Parse(num_1[i].ToString());
+                }
+
+                for (int i = 0; i < number_1.Length; i++)
+                {
+                    Console.Write("{0} ", number_1[i]);
+                }
+            } else if (num_1[0] == '\n')
+            {
+                Console.WriteLine("Вы не ввели первое число");
+            }
+
+            Console.WriteLine();
+
+            number_2 = new int[num_2.Length];
+            for (int i = 0; i < num_2.Length; i++)
+            {
+                number_2[i] = int.Parse(num_2[i].ToString());
+            }
+
+            for (int i = 0; i < number_2.Length; i++)
+            {
+                Console.Write("{0} ", number_2[i]);
+            }
+        }
+
+        public void Plus()
+        {
+
+        }
+    }
+}
--- Classes/Program.cs
+++ Classes/Program.cs
@@ -10,6 +10,29 @@ namespace Classes
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Выберите нужное действие: ");
+            Console.WriteLine("1. Большое целое число");
+            Console.WriteLine("2. Большая дробь");
+            Console.Write("Выбор: ");
+            int number = int.Parse(Console.ReadLine());
+
+            switch (number){
+                case 1:
+                    Console.Write("Введите первое число: ");
+                    string num_1 = Console.ReadLine();
+
+                    Console.Write("Введите второе число: ");
+                    string num_2 = Console.ReadLine();
+
+                    Large_Integer large = new Large_Integer(num_1, num_2);
+                    large.Plus();
+
+                    break;
+                case 2:
+                    Console.WriteLine("*Данный режим находится в стадии разработки*");
+                    break;
+            }
+            Console.ReadKey();
         }
     }
 }
