50d7d6559daf1379a5e20d91d6210b2a0bc6823a
Mineralo4ka <andrew.pavlenko@mail.ru>
2018-03-12 18:03:25 +0700

    Fixed func Plus and Add reverse numbers

--- Classes/Large_Integer.cs
+++ Classes/Large_Integer.cs
@@ -15,9 +15,9 @@ namespace Classes
             number = new int[num.Length];
             if (num.Length != 0 )
             {
-                for (int i = 0; i < num.Length; i++)
+                for (int i = num.Length - 1; i >= 0; i--)
                 {
-                    number[i] = int.Parse(num[i].ToString());
+                    number[number.Length - i - 1] = int.Parse(num[i].ToString());
                 }
             } else
             {
@@ -34,51 +34,61 @@ namespace Classes
             }
         }
 
-        public static Large_Integer operator +(Large_Integer number_1, Large_Integer number_2)
+        public static Large_Integer operator + (Large_Integer number_1, Large_Integer number_2)
         {
             Large_Integer result = null;
 
             if (number_1.number.Length > number_2.number.Length)
             {
-                result = new Large_Integer(number_1.number.Length);
+                result = new Large_Integer(number_1.number.Length + 1);
             }
 
             if (number_2.number.Length > number_1.number.Length)
             {
-                result = new Large_Integer(number_2.number.Length);
+                result = new Large_Integer(number_2.number.Length + 1);
             }
 
             if (number_1.number.Length == number_2.number.Length)
             {
-                result = new Large_Integer(number_1.number.Length);
+                result = new Large_Integer(number_1.number.Length + 1);
             }
 
-            for (int i = 0; i < number_1.number.Length; i++)
+            for (int i = 0; i <= number_1.number.Length - 1; i++)
             {
-                Console.Write("{0} ", number_1.number[i]);
+                Console.Write(number_1.number[i]);
             }
             Console.WriteLine();
-            for (int i = 0; i < number_2.number.Length; i++)
+            for (int i = 0; i <= number_2.number.Length - 1; i++)
             {
-                Console.Write("{0} ", number_2.number[i]);
+                Console.Write(number_2.number[i]);
             }
             Console.WriteLine();
 
-            double c = 0;
-            for (int i = result.number.Length; i > 0; i--)
+            int digit;
+            
+            for (int i = 0; i < result.number.Length - 1; i++)
             {
-                //result.number[i] = number_1.number[i] + number_2.number[i];
-                if (number_1.number[i] + number_2.number[i] > 10)
+                digit = number_1.number[i] + number_2.number[i];
+                if (digit >= 10)
                 {
-                    c = number_1.number[i] + number_2.number[i];
-                    result.number[i] = Convert.ToInt16( c % 10 );
-                    result.number[i - 1] = number_1.number[i] + number_2.number[i];
+                    result.number[i] = digit - 10;
+                    result.number[i + 1] += 1;
+                } else
+                {
+                    result.number[i] = digit;
                 }
             }
 
-            for (int i = 0; i < result.number.Length; i++)
+            result.number.Reverse();
+            for (int i = 0; i <= result.number.Length - 1; i++)
             {
-                Console.Write("{0} ", result.number[i]);
+                if (result.number[0] == 0)
+                {
+                    Console.Write("");
+                } else
+                {
+                    Console.Write(result.number[i]);
+                }
             }
 
             return result;
